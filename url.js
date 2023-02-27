ae639e1785fce8b33e70dd4586e42930dcb64b28
PPPPSY <120995066+PPPPSY@users.noreply.github.com>
2023-02-28 07:07:08 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,6 +10,5 @@
 const urls = [
 	'https://www.surveycake.com/s/bpnG9',
 	'https://www.surveycake.com/s/9qWvq',
-	'https://www.surveycake.com/s/6yLMX',
         'https://www.surveycake.com/s/4LweQ'
 ];
